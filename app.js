d5c79c4f5581dc1dadc310e322ac7f743055e04e
MCarmen <detoro.mcarmen@gmail.com>
2018-05-14 10:43:53 +0200

    getTransformStream already implemented.

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
 var http = require('http');
 var request = require('request');
 var cheerio = require ('cheerio');
-var { Transform } = require('stream');
+var stream = require('stream');
 var fs = require('fs');
 var path = require('path');
-var {URL} = require('url');
+var url = require('url');
 var archiver = require('archiver');
-var urlMod = require('url');
 
-/* 
-// Aquest codi l'heu de posar allà a on toqui.
+
+function getTransformStream(url, recLevel, replaceManager, downloadedFiles, doCrawlAndDownloadResource) {
+  var transformStream = new stream.Transform();
+  var buffer='';
+
   transformStream._transform = function(chunk, encoding, callback) {    
     buffer += chunk.toString();
-    //TODO: invocar a la callback
+    callback();
   };
-
-
 
   transformStream._flush = function(callback){
     this.push(transformStream._replace(buffer));
-    //TODO: invocar al a callback
+    callback();
   }
-
-  
 
   transformStream._replace = function(chunk){
       $ = cheerio.load(chunk);
-      $('a').each(function (i, link){      
+      $('a').each(function (i, link){
         var newUrl = $(this).attr('href'); 
-        //TODO: inicialitzar la variable newUrlName
-        $(this).attr('href', newUrlName);      
+        var newUrlName = replaceManager.lookupName(newUrl);
+        $(this).attr('href', newUrlName);
+
+        doCrawlAndDownloadResource(
+          URLManager.getDownloadableURL(url, newUrl),
+          recLevel - 1, replaceManager, newUrlName, downloadedFiles); 
       }); //end $a.each
       return $.html();
-  }
-*/
+  }; 
+
+  return transformStream;  
+}
 
 
+function routeRequests(req, res){
+  console.log("req.url: " + req.url);
+
+  // TODO
+}
+
+http.createServer(routeRequests).listen(8081);
